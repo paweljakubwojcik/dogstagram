bb70c62f768b6769ca565b749fe502ec03c662d3
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2021-03-12 23:52:23 +0100

    added swipe gestures

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,12 +8,14 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import Profile from './main/Profile'
 import Feed from './main/Feed'
+import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures'
+import { Container } from './styles/commonStyles'
 
 const Tab = createBottomTabNavigator()
 
 const Dummy = () => null
 
-export default function Main() {
+export default function Main({ navigation }) {
     const dispatch = useDispatch()
     const { currentUser } = useSelector((store) => ({
         currentUser: store.userState.currentUser,
@@ -24,41 +26,50 @@ export default function Main() {
     }, [])
 
     return (
-        <Tab.Navigator>
-            <Tab.Screen
-                name="Feed"
-                component={Feed}
-                options={{
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="home" color={color} size={26} />
-                    ),
-                }}
-            />
-            <Tab.Screen
-                name="Add-dummy"
-                component={Dummy}
-                options={{
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="plus-box" color={color} size={26} />
-                    ),
-                    tabBarLabel: 'Add',
-                }}
-                listeners={({ navigation }) => ({
-                    tabPress: (event) => {
-                        event.preventDefault()
-                        navigation.navigate('Add')
-                    },
-                })}
-            />
-            <Tab.Screen
-                name="Profile"
-                component={Profile}
-                options={{
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="account-circle" color={color} size={26} />
-                    ),
-                }}
-            />
-        </Tab.Navigator>
+        <GestureRecognizer
+            onSwipeRight={() => {
+                navigation.push('Add')
+            }}
+            style={{
+                flex: 1,
+            }}
+        >
+            <Tab.Navigator>
+                <Tab.Screen
+                    name="Feed"
+                    component={Feed}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons name="home" color={color} size={26} />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Add-dummy"
+                    component={Dummy}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons name="plus-box" color={color} size={26} />
+                        ),
+                        tabBarLabel: 'Add',
+                    }}
+                    listeners={({ navigation }) => ({
+                        tabPress: (event) => {
+                            event.preventDefault()
+                            navigation.navigate('Add')
+                        },
+                    })}
+                />
+                <Tab.Screen
+                    name="Profile"
+                    component={Profile}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons name="account-circle" color={color} size={26} />
+                        ),
+                    }}
+                />
+            </Tab.Navigator>
+        </GestureRecognizer>
     )
 }
